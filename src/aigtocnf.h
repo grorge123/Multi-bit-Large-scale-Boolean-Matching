cd3200b448541d644ccdc144c475ddcac0eabfcd
grorge <g97116@yahoo.com>
2023-05-16 13:54:19 +0800

    feat: add aigtocnf

---
+++ src/aigtocnf.h
@@ -0,0 +1,16 @@
+//
+// Created by grorge on 5/16/23.
+//
+
+#ifndef MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_AIGTOCNF_H
+#define MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_AIGTOCNF_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// C struct declaration
+void aigtocnf(char * fileName, char * outputName);
+#ifdef __cplusplus
+}
+#endif
+#endif //MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_AIGTOCNF_H
