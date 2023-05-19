0921c572356c3f3d49d8c6d9d5ce2b450fb681db
grorge <g97116@yahoo.com>
2023-05-19 13:38:12 +0800

    feat: init largeScale

---
+++ src/largeScale.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Han Wen Tsao on 2023/5/19.
+//
+
+#include "largeScale.h"
+#include <fstream>
+
+using namespace std;
+
+void startLargeScaleBooleanMatching(string inputPath, string outputPath) {
+
+    ifstream myfile;
+    myfile.open("shopping_list.txt");
+
+}
---
+++ src/largeScale.h
@@ -0,0 +1,12 @@
+//
+// Created by Han Wen Tsao on 2023/5/19.
+//
+
+#ifndef MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_LARGESCALE_H
+#define MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_LARGESCALE_H
+
+#include <string>
+
+void startLargeScaleBooleanMatching(string inputPath, string outputPath);
+
+#endif //MULTI_BIT_LARGE_SCALE_BOOLEAN_MATCHING_LARGESCALE_H
